062306c8fa4c8a491bf27c2bd8c16a7257d5a74b
Tamara Montijo <tamaramontijo@wizsolucoes.com.br>
2023-06-22 12:20:29 -0300

    task 4

--- task_04/script.js
+++ task_04/script.js
@@ -9,4 +9,12 @@ timeline
     gsap.to('.white-background', { y: '-100%' })
   )
   .to('.warsaw', {scale: 1.2, duration: 3})
+  .to('.madrid', { opacity: 1 })
+  .from('.madrid', { x: '100%' })
+  // .add(() => {
+  //   gsap.from('.madrid', { x: '-100%' });
+  // })
+  .to('.madrid', {scale: 1.2, duration: 3})
+
+  
   
