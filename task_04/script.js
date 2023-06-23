1bc9090b283a84bfc4caf9be0d89ebcd70db0333
Tamara Montijo <tamaramontijo@wizsolucoes.com.br>
2023-06-22 21:49:03 -0300

    task 4

--- task_04/script.js
+++ task_04/script.js
@@ -8,16 +8,90 @@ timeline
     timeline.to('.yellow-background', { y: '-100%' }),
     gsap.to('.white-background', { y: '-100%' })
   )
-  .to('.warsaw', {scale: 1.2, duration: 3})
+
+  // WARSAW
+
+  .to('.warsaw', {scale: 1.2, duration: 3, delay: 0})
+
+  .to('.country', { opacity: 1, delay: 0.5 }, "-=3")
+  .from('#country-p', { opacity: 0, y: '100%', delay: 0.5 }, "-=2.9")
+  
+  .to('.city', { opacity: 1, delay: 1 }, "-=2.5")
+  .from('#city-p', { opacity: 0, y: '100%', delay: 1 }, "-=2.4")
+
+  .to('.book-now', { opacity: 1 }, "-=2.5")
+  .from('.book-now', { x: '150%', delay: 1}, "-=2.5")
+  .to('.price', { opacity: 1, delay: 1 }, "-=2.5")
+  .from('.price', { y: '150%', delay: 1}, "-=2.5")
+
+  // CLEAR INFOS
+  .set('.country', { clearProps: 'all' })
+  .set('.city', { clearProps: 'all' })
+  .set('.book-now', { clearProps: 'all' })
+  .set('.price', { clearProps: 'all' })
+    
+    
+    
+  // MADRID
   .to('.madrid', { opacity: 1 })
   .from('.madrid', { x: '100%' })
-  // .add(() => {
-  //   gsap.from('.madrid', { x: '-100%' });
-  // })
-  .to('.madrid', {scale: 1.2, duration: 3})
+
+  // CHANGE TEXTS
+
+  .to('#country-p', { textContent: "Spain" })
+  .to('#city-p', { textContent: "Madrid" })
+  .to('#price-strong', { textContent: "295 USD" })
+
+  .to('.madrid', {scale: 1.2, duration: 3, delay: 0})
+
+  .to('.country', { opacity: 1, delay: 0.5 }, "-=3")
+  .from('#country-p', { opacity: 0, y: '-100%', delay: 0}, "-=2.9")
+  .to('#country-p', { opacity: 1, y: '0%', delay: 0.5 }, "-=2.5")
+
+  .to('.city', { opacity: 1, delay: 0.5 }, "-=2.5")
+  .from('#city-p', { opacity: 0, y: '-100%', delay: 0 }, "-=2.3")
+  .to('#city-p', { opacity: 1, y: '0%', delay: 0.5 }, "-=2.3")
+
+  .from('.book-now', {opacity: 0, x: '150%', delay: 1}, "-=2.5")
+  .to('.book-now', {opacity: 1, x: '0%', delay: 1}, "-=2.3")
+  
+  .from('.price', { opacity: 0, y: '150%', delay: 1}, "-=2.2")
+  .to('.price', { opacity: 1, y: '0%', delay: 1 }, "-=2.2")
+
+  // CLEAR INFOS
+  .set('.country', { clearProps: 'all' })
+  .set('.city', { clearProps: 'all' })
+  .set('.book-now', { clearProps: 'all' })
+  .set('.price', { clearProps: 'all' })
+
+  // BERLIN
   .to('.berlin', { opacity: 1 })
   .from('.berlin', { x: '100%' })
-  .to('.berlin', {scale: 1.2, duration: 3})
+
+  // CHANGE TEXTS
+
+  .to('#country-p', { textContent: "Germany" })
+  .to('#city-p', { textContent: "Berlin" })
+  .to('#price-strong', { textContent: "350 USD" })
+
+  .to('.berlin', {scale: 1.2, duration: 3, delay: 0})
+
+  // INFOS
+
+  .to('.country', { opacity: 1, marginRight: '172px', delay: 0.5 }, "-=3")
+  .from('#country-p', { opacity: 0, y: '100%', delay: 0}, "-=2.9")
+  .to('#country-p', { opacity: 1, y: '0%', delay: 0.5 }, "-=2.5")
+
+  .to('.city', { opacity: 1, delay: 0.5 }, "-=2.5")
+  .from('#city-p', { opacity: 0, y: '100%', delay: 0 }, "-=2.5")
+  .to('#city-p', { opacity: 1, y: '0%', delay: 0.5 }, "-=2.5")
+
+  .from('.book-now', {opacity: 0, x: '150%', delay: 1}, "-=2.5")
+  .to('.book-now', {opacity: 1, x: '0%', delay: 1}, "-=2.3")
+  
+  .from('.price', { opacity: 0, y: '150%', delay: 1}, "-=2.2")
+  .to('.price', { opacity: 1, y: '0%', delay: 1 }, "-=2.2")
+  
 
   
   
